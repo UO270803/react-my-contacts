7da0bd84de41dd34f36df7ae9b9851bca349c7be
Marcos Fernandez <UO270803@uniovi.es>
2023-05-03 19:35:41 +0200

    Fin del desarrollo de la aplicación, lista para despliegue

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import CardList from "./components/CardList";
+import Scroll from "./components/Scroll";
+import Searcher from "./components/Searcher";
+import ErrorFallback from "./components/ErrorFallback";
+
+import { ErrorBoundary } from "react-error-boundary";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const [searchField, setSearchField] = useState('');
+
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
+  }
+
+  const searchedContacts = contacts.filter(contact => {
+      return (contact.name['first'] + ' ' + contact.name['last']).toLowerCase().includes(searchField.toLowerCase())
+  });
+
+  const onAZ = () => {
+    let az = contacts.sort((a, b) => {
+      return (a.name['first'] + " " + a.name['last']).localeCompare(b.name['first'] + " " + b.name['last'])
+    })
+    setContacts([...az]); //clone the list
+  }
+
+  const onZA = () => {
+    let za = contacts.sort((a, b) => {
+      return (b.name['first'] + " " + b.name['last']).localeCompare(a.name['first'] + " " + a.name['last'])
+    })
+    setContacts([...za]); //clone the list
+  }
+
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=20')
+      .then(response => response.json())
+      .then(contacts => setContacts(contacts.results));
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='tc '>
+      <header>
+        <h1 className='f1'>My contacts</h1>
       </header>
+      {contacts.length === 0 ? <h2 className='f2'>Loading...</h2> :
+        <ErrorBoundary>
+          <Searcher searchChange={onSearchChange} az={onAZ} za={onZA}/>
+          <Scroll>
+            <CardList contacts={searchedContacts} />
+          </Scroll>
+        </ErrorBoundary>
+      }
+      <footer>
+        <hr /><p>Desarrollo de Software para Dispositivos Moviles.
+          {new Date().getFullYear()}</p>
+      </footer>
     </div>
+
   );
 }
 
